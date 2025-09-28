9b314806178534471124243c9cc4de3b691889fc
Harshwerdhan <harsh24314@gmail.com>
2025-09-29 01:05:15 +0530

    Update .yml file

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ firebase = pyrebase.initialize_app(firebase_config)
 pb_auth = firebase.auth()
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
+cred = credentials.Certificate(os.getenv("FIREBASE_SERVICE_ACCOUNT"))
 firebase_admin.initialize_app(cred)
 
 
